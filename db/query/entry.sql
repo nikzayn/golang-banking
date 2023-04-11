5a54b01b0b70ee94efbda3e11e93a15dbb594d78
nikzayn <nikhilvaidyar1997@gmail.com>
2023-04-11 23:36:50 +0530

    entry and transfer sql queries --added

---
+++ db/query/entry.sql
@@ -0,0 +1,15 @@
+INSERT INTO entries (
+    account_id,
+    amount
+) VALUES (
+    $1, $2
+) RETURNING *;
+
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+SELECT * FROM entries
+WEHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
