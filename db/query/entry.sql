9f69b6b715340f9bcffe5e5c7f50b38101e349dd
nikzayn <nikhilvaidyar1997@gmail.com>
2024-08-24 20:46:18 +0530

    sql queries --update

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,15 +1,10 @@
-INSERT INTO entries (
-    account_id,
-    amount
-) VALUES (
-    $1, $2
-) RETURNING *;
+INSERT INTO entries (account_id, amount) VALUES ($1, $2) RETURNING *;
 
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
 
 SELECT * FROM entries
-WEHERE account_id = $1
+WHERE account_id = $1
 ORDER BY id
 LIMIT $2
 OFFSET $3;
